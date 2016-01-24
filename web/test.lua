4c9352666c4da05dc42837c4d2a5080cdc93b905
John MacFarlane <jgm@berkeley.edu>
2016-01-24 14:52:48 -0800

    Improved web test.

--- web/test.lua
+++ web/test.lua
@@ -3,11 +3,39 @@ local to_commonmark = require 'to_commonmark'
 local connector = require "wsapi.mock"
 
 local app = connector.make_handler(to_commonmark.run)
+local failed = 0
 
-do
-  local response, request = app:post("/", {html = "<em>hi</em>"}, {})
-  assert(response.code                    == 200)
-  assert(response.headers["Content-type"] == "text/plain; charset=UTF-8")
-  assert(response.body                    == "*hi*\n")
+local function dotest(name, method, params, headers, expected)
+  io.write(name .. '...')
+  local response, request
+  if method == 'GET' then
+    response, request = app:get("/", params, headers)
+  elseif method == 'POST' then
+    response, request = app:post("/", params, headers)
+  else
+    assert(false)
+  end
+  if response.headers["Content-type"] ~= "text/plain; charset=UTF-8" then
+    io.write('FAILED\nContent-type is ' ..
+       response.headers["Content-type"] .. '\n')
+    failed = failed + 1
+    return false
+  end
+  for k,v in pairs(expected) do
+    if response[k] ~= v then
+      io.write('FAILED\n' .. k .. ' was "' .. response[k] ..
+          '", expected "' .. v .. '"\n')
+      failed = failed + 1
+      return false
+    end
+  end
+  io.write('OK\n')
+  return true
 end
 
+dotest('POST with html', 'POST', {html = '<em>hi</em>'}, {},
+  {code = 200, body = '*hi*\n'})
+dotest('GET with html', 'GET', {html = '<em>hi</em>'}, {},
+  {code = 200, body = '*hi*\n'})
+
+os.exit(failed)
